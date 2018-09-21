1dd67aa968578e2a71ea715e074a4fd7888a4e2e
학과 게임 <hoseogame@hoseogame.onmicrosoft.com>
2018-09-21 11:32:08 +0900

    9/21 PP05.AnimatingSprite

---
+++ PP05.AnimatingSprite/Game.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include "SDL.h"
+
+class Game
+{
+public:
+	Game() {}
+	~Game() {}
+	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+	bool running() { return m_bRunning; }
+private:
+	SDL_Texture * m_pTexture;
+	SDL_Rect m_sourceRectangle;
+	SDL_Rect m_destinationRectangle;
+	SDL_Window * m_pWindow=0;
+	SDL_Renderer*m_pRenderer=0;
+	bool m_bRunning = true;
+};

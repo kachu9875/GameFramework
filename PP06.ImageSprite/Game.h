7d96ff1a1d9eecdb5d0a0a9e6aa4bb623edea1ac
ohgyukwon <20171106@vision.hoseo.edu>
2018-10-11 14:33:33 +0900

    10/11 HH.04

--- PP06.ImageSprite/Game.h
+++ PP06.ImageSprite/Game.h
@@ -16,9 +16,17 @@ public:
 	bool running() { return m_bRunning; }
 private:
 	SDL_Texture * m_pTexture;
+	SDL_Texture * m_pTexture2;
+	SDL_Texture * m_pTexture3;
 	SDL_Rect m_sourceRectangle;
 	SDL_Rect m_destinationRectangle;
+	SDL_Rect m_sourceRectangle2;
+	SDL_Rect m_destinationRectangle2;
+	SDL_Rect m_sourceRectangle3;
+	SDL_Rect m_destinationRectangle3;
 	SDL_Window * m_pWindow = 0;
 	SDL_Renderer*m_pRenderer = 0;
+	SDL_Renderer*m_pRenderer2 = 0;
+	SDL_Renderer*m_pRenderer3 = 0;
 	bool m_bRunning = true;
 };
